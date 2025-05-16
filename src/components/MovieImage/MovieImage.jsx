b2596e258757445db85025d35fda92f9102bd431
dimakisiv <dimakisiv@gmail.com>
2025-05-16 22:12:07 +0300

    added default image

--- src/components/MovieImage/MovieImage.jsx
+++ src/components/MovieImage/MovieImage.jsx
@@ -1,6 +1,9 @@
 import imagesService from "../../services/images.service";
 import styles from "./MovieImage.module.css";
 
+const defaultImg =
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+
 export function MovieImage({
   image,
   pathKey = "poster_path",
@@ -9,6 +12,6 @@ export function MovieImage({
 }) {
   const path = image?.[pathKey];
   const alt = image?.[altKey] || "";
-  if (!path) return null;
-  return <img className={styles.img} src={imagesService.getImageUrl(path, size)} alt={alt} />;
+  const src = path ? imagesService.getImageUrl(path, size) : defaultImg;
+  return <img className={styles.img} src={src} alt={alt} />;
 }
